869567ec54d123e6b37c41aab2f2e8b0244cb6b5
Bradley Lyman <lyman.brad3211@gmail.com>
2021-05-02 21:42:37 -0700

    remove memory sample mask
    because:
    - using a temporary &[] for the multisample mask is undefined
      behavior!
    - the temp &[] was causing intermittent blank-screen errors for
      release builds!

--- src/graphics/draw2d/graphics_pipeline.rs
+++ src/graphics/draw2d/graphics_pipeline.rs
@@ -112,7 +112,6 @@ impl GraphicsPipeline {
                 .sample_shading_enable(false)
                 .rasterization_samples(vk::SampleCountFlags::TYPE_1)
                 .min_sample_shading(1.0)
-                .sample_mask(&[])
                 .alpha_to_coverage_enable(false)
                 .alpha_to_one_enable(false);
 
